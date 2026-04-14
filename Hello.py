234d13f819eed3fb81bdc3ac340f2b6858dd07dc
orlando260 <orlando.kisvarday@gmail.com>
2026-04-14 11:08:48 +0200

    Ulitmo di Hello

--- Hello.py
+++ Hello.py
@@ -457,7 +457,7 @@ class MovingAvarage():
 moving_avarage=MovingAvarage(4)
 result=moving_avarage.compute([6,5,1,3,4])
 '''
-
+'''
 class CSVTimeSeriesFile():
     def __init__(self,name):
         self.file=name
@@ -486,18 +486,366 @@ def compute_variation(time_series,inizio,fine):
                 somma=somma+b
             mean=somma/j
         dict8['i']=mean
-        dict88={}
-        dict88['{i}-{k}']= dict8[i]-dict8[i+1]
+    dict88={}
+    for i in range(inizio,fine):
+            dict88['{i}-{k}']= dict8[i]-dict8[i+1]
     return dict88
+'''
+'''
+class CSVfile():
+    def __init__(self,CSV_file):
+        if(type(CSV_file)==str):
+            self.name=CSV_file
+        else :
+            print("sta roba non è una stringa")
+            self.name=None
+    def get_data(self):
+        try:
+            lista=[]
+            with open(self.name,'r') as file:
+                for line in file:
+                    list=[]
+                    line=line.replace(","," ").replace("\n","")
+                    for i in line.split(" "):
+                        list.append(i)
+                    lista.append(list)
+        except FileNotFoundError as e:
+            print("sto file {} non esiste".format(self.name))
+            print("e hai avuto sto errore:{}".format(e))
+        return (lista)
+    
+class NumericalCSVfile(CSVfile):
+    def __init__(self,CSV_file):
+        super().__init__(CSV_file)
+    def get_data(self):
+        csv_file=super().get_data()
+        for element in csv_file:
+            try:
+                element[1]= float(element[1])
+            except:
+                print("non posso convertire questo elemento ({}) in float".format(element[1]))
+        return csv_file
 
+    
+
+
+file=NumericalCSVfile("shampoo_sales.csv")
+print(f"{file.get_data()}")
+'''
+'''
+import datetime
+class Age():
+    def __init__(self,day,month,year):
+        self.year=year
+        self.month=month
+        self.day=day
+        self.Oggi=datetime.datetime.now()
+    def get_age(self):
+        anni=self.Oggi-datetime.datetime(self.year,self.month,self.day)
+        return int(anni.days/365)
+    def prox_comple(self):
+        diff=datetime.datetime((self.year+self.get_age()+1),self.month,self.day)-self.Oggi
+        return diff
+
+eta=Age(29,11,2006)
+print(f"{eta.get_age()} anni")
+print(eta.prox_comple())
+'''
+'''
+a=0
+while(a==0):
+    a=input("inserire un numero: ")
+    try:
+        a=int(a)
+    except Exception as e:
+        print("{} non è un numero".format(a))
+        print("ho avuto quindi questo errore {}".format(e))
+        a=0
+print(a*a)
+'''
+'''
+while(True):
+    print("seleziona\n1 per sommare due numeri\n" \
+    "2 per fare una sottrazione tra due numeri\n" \
+    "3 per uscire")
+    var=input()
+    try:
+        var=int(var)
+    except Exception as e:
+        print("{} non è un numero".format(var))
+        print("ho avuto quindi questo errore {}".format(e))
+    if var==1:
+        uno=input("primo numero: ")
+        due=input("secondo numero: ")
+        try:
+            uno=int(uno)
+        except Exception as e:
+            print("{} non è un numero".format(uno))
+            print("ho avuto quindi questo errore {}".format(e))
+        try:
+            due=int(due)
+            uno=uno+due
+            print(uno)
+        except Exception as e:
+            print("{} non è un numero".format(due))
+            print("ho avuto quindi questo errore {}".format(e))
         
+    if var==2:
+        uno=input("primo numero: ")
+        due=input("secondo numero: ")
+        try:
+            uno=int(uno)
+        except Exception as e:
+            print("{} non è un numero".format(uno))
+            print("ho avuto quindi questo errore {}".format(e))
+        try:
+            due=int(due)
+            uno=uno-due
+            print(uno)
+        except Exception as e:
+            print("{} non è un numero".format(due))
+            print("ho avuto quindi questo errore {}".format(e))
+        
+    if var==3:
+        break
+'''
+'''
+class ExamException(Exception):
+    pass
+
+
+class CSVTimeSeriesFile():
+    def __init__(self,nome):
+        self.nome=nome
+        try:
+            with open(self.nome, 'r') as file:
+                next(file)
+        except:
+            raise ExamException("Errore: non legge il file")
+    def get_data(self):
+        lista=[]
+        try:
+            with open(self.nome, 'r') as file:
+                next(file)
+                for line in file:
+                    List=[]
+                    for i in line.split(","):
+                        try:
+                            List.append(float(i.replace("\n","")))
+                        except:
+                            List.append(i.replace("\n",""))
+                    lista.append(List)
+            return lista
+        except Exception as e:
+            print("il file {} non esiste".format(self.nome))
+            print("di conseguenza c'è stato questo errore: {}".format(e))
+
+def compute_variation(Data, first_year,last_year,N):
+    Diz={}
+    lista={}
+
+    for i in range(first_year,last_year+1):
+        somma=0
+        media=0
+        n=0
+        
+        for line in Data:
+            
+            if (any(str(i) in str(k) for k in line)):
+                somma=somma+line[1]
+                n=n+1
+
+        lista[i]=somma/n
+        for j in range(1,N+1):
+            Somma=0
+            m=0
+            for line in Data:
+                if line[0].startswith(str(i-j)):
+                    Somma=Somma+line[1]
+                    m=m+1
+            if m!=0:
+                media=media+Somma/m
+        media=media/N
+        if i-first_year>= N:
+            Diz[i]=lista[i]-media
+    return Diz
+            
+                
             
 
-                
 
 
-                
-                
+
+
+Nome= "../Downloads/GlobalTemperatures.csv"
+Data=CSVTimeSeriesFile(Nome)
+Data2=Data.get_data()
+print(compute_variation(Data2,1900,1904,3))
+
+'''
+'''
+class ExamException(Exception):
+    pass
+
+class CSVTimeSeriesFile():
+    def __init__(self,nome):
+        self.nome=nome
+        try:
+            with open(self.nome, 'r') as file:
+                try:
+                    next(file)
+                except Exception as e:
+                    raise ExamException("File non leggibile a causa del seguente errore:{}".format(e))
+        except Exception as e:
+            raise ExamException("File non apribile a causa del seguente errore:{}".format(e))
+    def get_data(self,paese):
+        lista=[]
+        with open(self.nome) as file:
+            for line in file:
+                line=line.replace("\n","")
+                try:
+                    a,b,c=line.split(",")
+                    if c==paese:
+                        try:
+                            lista.append([a,float(b)])
+                        except:
+                            continue
+                except:
+                    continue
+        if lista==[]:
+            return ("paese non presente nel file")
+        return lista
+    
+
+def compute_variation(time_series1,time_series2,first_year,last_year):
+    try:
+        first_year=int(first_year)
+    except:
+        raise ExamException("numero non intero")
+        
+    diz={}
+    Diz1={}
+    Diz2={}
+    Diz3={}
+    Diz4={}
+    for anno in range(first_year, last_year +1):
+        media=0
+        Media=0
+        somma=0
+        Somma=0
+        Diz1[anno]=[elem[1] for elem in time_series1 if elem[0].startswith(str(anno))]
+        Diz2[anno]=[elem[1] for elem in time_series2 if elem[0].startswith(str(anno))]
+        for elem in Diz1[anno]:
+            somma=somma+elem
+            media=media+1
+        Diz3[anno]=somma/media
+        for elem in Diz2[anno]:
+            Somma=Somma+elem
+            Media=Media+1
+        Diz4[anno]=Somma/Media
+        diz[anno]=Diz4[anno]-Diz3[anno]
+    if diz!={}:
+        return diz
+    else:
+        raise ExamException("Errore: non è un intervallo con valori validi")
+        
+    
+
+
+
+    
+data=CSVTimeSeriesFile("../Downloads/GlobalLandTemperaturesByCountry.csv")
+uno=data.get_data("Italy")
+due=data.get_data("Japan")
+
+print(compute_variation(uno,due,1945,1950))
+'''
+
+class ExamException(Exception):
+    pass
+
+class CSVTimeSeries():
+    def __init__(self,nome):
+        self.nome=nome
+        try:
+            with open(self.nome) as file:
+                try:
+                    next(file)
+                except Exception as e:
+                    raise ExamException("Errore: {}. File non leggibile".format(e))
+        except Exception as e:
+            raise ExamException("Errore: {}. File non esistente".format(e))
+    
+    def get_data(self, paese):
+        lista=[]
+        with open(self.nome) as file:
+            for line in file:
+                try:
+                    a,b,c=line.replace("\n","").split(",")
+                    if c==paese:
+                        try:
+                            lista.append([a,float(b)])
+                        except:
+                            continue
+                except:
+                    continue
+        if lista==[]:
+            raise ExamException("paese non presente nel file")
+        return lista
+    
+data=CSVTimeSeries("../Downloads/aaa.csv")
+time_series=data.get_data("Rome")
+print(time_series)
+
+def compute_slope(time_series, first_year, last_year):
+    diz1={}
+    if first_year>last_year:
+        raise ExamException("intervallo non valido")
+    for anno in range(first_year,last_year +1):
+        lista=[]
+        n=0
+        for elem in time_series:
+            if elem[0].startswith(str(anno)):
+                lista.append(elem[1])
+                n=n+1
+        if n>=6:
+            diz1[anno]=lista
+    if diz1=={}:
+        raise ExamException("intervallo non esistente")
+    media_diz={}
+    for anno in diz1:
+        media=0
+        n=0
+        for elem in diz1[anno]:
+            media=media+elem
+            n=n+1
+        media_diz[anno]=media/n
+    print(media_diz)
+    x=0
+    y=0
+    z=0
+    for anno in diz1:
+            x=x+anno
+            z=z+1
+            y=y+media_diz[anno]
+    if z==0:
+        raise ExamException("C'è qualche errore su n")
+    xedia=x/z
+    yenia=y/z
+    m1=0
+    m2=0
+    for anno in diz1:
+        m1=m1+(anno-xedia)*(media_diz[anno]-yenia)
+        m2=m2+(anno-xedia)**2
+    if m2==0:
+        raise ExamException("C'è qualche errore con il denominatore")
+    return m1/m2
+
+
+
+
+print(compute_slope(time_series,1900,1902))
+
 
 
 
